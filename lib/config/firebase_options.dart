eadaac2bce2b9381b7c125e208ac2456dffbb4f4
laizin <laizin2107@gmail.com>
2024-10-08 14:07:45 +0400

    addede firebase options

---
+++ lib/config/firebase_options.dart
@@ -0,0 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
+
+FirebaseOptions getFirebaseOptions() {
+  return FirebaseOptions(
+    apiKey: "AIzaSyCRREnMu-hBCdKujy-t-IBUU8eN0HPiSPo",
+    projectId: "finmate-d9f70",
+    storageBucket: "finmate-d9f70.appspot.com",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    appId: "604761908212",
+  );
+}
